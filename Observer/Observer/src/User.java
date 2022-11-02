271e9923245fcd3f3fe066e033a386774340b62d
QuangPV10 <phanmvanquang.work@gmail.com>
2022-11-02 13:46:29 +0700

    observer

---
+++ Observer/Observer/src/User.java
@@ -0,0 +1,45 @@
+
+enum LoginStatus {
+	SUCCESS, FAILURE, INVALID, EXPIRED
+}
+
+public class User {
+	private String email;
+	private String ip;
+	private LoginStatus status;
+
+	public User() {
+	};
+
+	public User(String email, String ip, LoginStatus status) {
+		super();
+		this.email = email;
+		this.ip = ip;
+		this.status = status;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public LoginStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(LoginStatus status) {
+		this.status = status;
+	}
+
+}
